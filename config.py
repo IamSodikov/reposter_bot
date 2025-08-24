fd46c46300166a9a95a11ce4ee96232cbfe06b0a
IamSodikov <mukhammadazizsodikov@gmail.com>
2025-08-24 17:33:59 +0500

    fix some bugs

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ SESSION_FOLDER = os.path.join(BASE_DIR, "data")
 INLINE_JSON_PATH = os.path.join(SESSION_FOLDER, "inline_keyboard_posts.json")
 INLINE_MEDIA_FOLDER = os.path.join(SESSION_FOLDER, "inline_media")
 
-API_ID = 13560279  # <-- Telegram API ID (https://my.telegram.org)
-API_HASH = "c61de11ee09f1a6b6b8e6d3ef7ab11e6"  # <-- Telegram API Hash
-BOT_TOKEN = "8167637162:AAGS5pWMxRSc3PJpl23RqoTme4kNlfSUsD0"  # <-- Bot token from @BotFather
+API_ID = "your api id"  # <-- Telegram API ID (https://my.telegram.org)
+API_HASH = "your api hash"  # <-- Telegram API Hash
+BOT_TOKEN = "your bot token"  # <-- Bot token from @BotFather
